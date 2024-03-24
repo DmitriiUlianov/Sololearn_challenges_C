47dd7252395b2cd52e363e8d28042680334c938c
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-03-24 20:58:21 +0100

    Update and rename Word_Rank.txt to Word_Rank.c

--- Word_Rank.c
+++ Word_Rank.c
@@ -1,4 +1,4 @@
-A word is a sequence of letters A-Z. Rearranging the letters in the word, you can come up with new words, composed of the same letters.  
+/* A word is a sequence of letters A-Z. Rearranging the letters in the word, you can come up with new words, composed of the same letters.  
 For example, the letters in the word TESTING can be rearranged to result in SETTING. 
 If we sort all the words made up of the same set of letters alphabetically, we can calculate the rank of each word.   
  
@@ -24,7 +24,7 @@ Explanation: Let's create a list of all the words that can be made up of the let
 4. BAAB 
 5. BABA 
 6. BBAA 
-The given word is number 2 in the list.
+The given word is number 2 in the list. */
 
 // Program to print all permutations of a string in sorted order.
 #include <stdbool.h>
