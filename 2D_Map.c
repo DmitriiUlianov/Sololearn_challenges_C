7b5831dbcec2f4f365f6293d776a05a15e0ec26a
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-03-24 20:51:34 +0100

    Update and rename 2D_Map.txt to 2D_Map.c

--- 2D_Map.c
+++ 2D_Map.c
@@ -1,4 +1,4 @@
-You're given a representation of a 5x5 2D map, and if you can only move left, right, up, or down, determine how many moves you would have to make to get between two points on the map. 
+/* You're given a representation of a 5x5 2D map, and if you can only move left, right, up, or down, determine how many moves you would have to make to get between two points on the map. 
  
 Task: 
  Determine the total number of moves that are needed between two points on a map.  The points that you move between are marked with a P and the spaces in between are marked with X. 
@@ -23,7 +23,7 @@ XXXXX
 XXXPX 
 XXXXX 
  
-You had to move right 2 spaces, then down 3 spaces for a total of 5 moves.
+You had to move right 2 spaces, then down 3 spaces for a total of 5 moves. */
 
 /*
 #include <stdio.h>
